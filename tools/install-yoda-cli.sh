40e6697dc8d44a1d1171ef0d781d0c0421b8b48f
yorkie <yorkiefixer@gmail.com>
2019-08-22 15:18:58 +0800

    update install script

--- tools/install-yoda-cli.sh
+++ tools/install-yoda-cli.sh
@@ -1,2 +1,6 @@
-curl https://github.com/yodaos-project/yoda-platform-tools/releases/download/v0.2.1/yoda-cli-v0.2.1-Darwin-x86_64.tar.gz > ./yoda-cli.tar.gz
 
+curl https://github.com/yodaos-project/yoda-platform-tools/releases/download/v0.2.1/yoda-cli-v0.2.1-Darwin-x86_64.tar.gz > ./yoda-cli.tar.gz
+tar xvzf ./yoda-cli.tar.gz
+
+pwd
+ls ./
